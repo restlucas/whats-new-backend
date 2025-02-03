6171701938face747c5e350acc4db9bb838bca53
restlucas <lucasouza.doliveira@gmail.com>
2025-02-03 18:34:01 -0300

    server changes

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
-const HOST = process.env.HOST || "localhost";
 
 const startServer = () => {
   try {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running at http://${HOST}:${PORT}/api`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server is running at http://0.0.0.0:${PORT}/api`);
     });
   } catch (error) {
     console.error("❌ Error starting server:", error);
