8ed71aa70cd90efbdb6c5646aafd70d23a755e8c
restlucas <lucasouza.doliveira@gmail.com>
2025-02-11 19:19:32 -0300

    options method

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: ["https://whats-new-rust.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
   })
