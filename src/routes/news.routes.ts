b6f4d99245ac38cc68d34d773971d43cd9dd37ba
restlucas <lucasouza.doliveira@gmail.com>
2025-02-13 13:13:00 -0300

    naming endpoint

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -18,10 +18,10 @@ router.get("/", getAllNews);
 router.get("/article", getFullArticle);
 router.get("/team/:teamId", authMiddleware, getResumeNewsByTeam);
 router.get("/edit/history/:teamId", authMiddleware, getEditHistoryByTeam);
-router.post("/:teamId/:userId", authMiddleware, createNews);
+router.post("/create/:teamId/:userId", authMiddleware, createNews);
 router.post("/comment/:userId/:newsId", authMiddleware, makeComment);
 router.post("/article/views", incrementViews);
-router.put("/", authMiddleware, updateNews);
+router.put("/update", authMiddleware, updateNews);
 router.delete("/:newsId", authMiddleware, deleteNews);
 
 export default router;
