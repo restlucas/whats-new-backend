c6a5b5d8686996462314459a805df885b50d4170
restlucas <lucasouza.doliveira@gmail.com>
2025-02-13 19:25:36 -0300

    samesite none

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -16,7 +16,7 @@ export const setRefreshTokenCookie = (res: Response, refreshToken: string) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
   });
 };
